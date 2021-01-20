22be5482330255952165345e40202d84da6807eb
Asen Yanyov <asen983@abv.bg>
2021-01-20 17:03:57 +0200

    open browser

---
+++ scripts/open_browser.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import webbrowser
+import time
+import platform
+import os
+
+urls = [
+    'abv.bg',
+    'google.com',
+    'aws.amazon.com',
+    'stackoverflow.com',
+    'pymotw.com/3/'
+        ]
+
+chrome_exe = "C://Program Files (x86)//Google//Chrome//Application//chrome.exe"
+webbrowser.register('chrome',
+	None,
+	webbrowser.BackgroundBrowser(chrome_exe))
+
+while True:
+    for i in urls:
+        webbrowser.get('chrome').open(i)
+        time.sleep(15)
+    
+    if platform.system() == 'Windows':
+        os.system("taskkill /f /im chrome.exe")
