9e66c683b961ab2da16fab8f40fb35cb18da6d82
Asen Yanyov <asen983@abv.bg>
2021-01-20 16:02:50 +0200

    add click

--- scripts/move_mouse.py
+++ scripts/move_mouse.py
@@ -1,6 +1,12 @@
 #pip install mouse
 import mouse
+import time
+
 # move 100 right and 100 down with a duration of 0.5 seconds
 while True:
     mouse.move(100, 100, absolute=False, duration=0.5)
+    time.sleep(2.5)
+    mouse.click('right')
     mouse.move(-100, -100, absolute=False, duration=0.5)
+    time.sleep(2.5)
+    mouse.click('right')
