8b0010b273f1a693d13904389523deb4c1526595
Asen Yanyov <asen983@abv.bg>
2021-01-20 11:54:17 +0200

    rm comments

--- scripts/move_mouse.py
+++ scripts/move_mouse.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 20 11:33:24 2021
-
-@author: asen.yanyov
-"""
-
 #pip install mouse
 import mouse
 # move 100 right and 100 down with a duration of 0.5 seconds
