81741cbba28ff7c95be83ef08dbc3dfc58dc52a5
Asen Yanyov <asen.yanyov@scalefocus.com>
2021-02-09 15:54:45 +0200

    update script

--- ECS_report_to_Excel/ECS_report_Excel.py
+++ ECS_report_to_Excel/ECS_report_Excel.py
@@ -14,21 +14,31 @@ for cluster, services in ecs_services.items():
     cl=cluster.split('/')[1]
     for svc in services:
         desc_serv = client.describe_services(cluster=cl, services=[svc]).get('services')
+        task_definition = desc_serv[0]['deployments'][0]['taskDefinition']
+        task_definition_info = client.describe_task_definition(taskDefinition = task_definition).get('taskDefinition')
         data.append({ "cluster": cl,
             "service": svc,
             "desired_count": desc_serv[0]['desiredCount'],
-            "load_balancer": desc_serv[0]['loadBalancers']
+            "load_balancer": desc_serv[0]['loadBalancers'],
+            "task_definition": task_definition,
+            "cpu": task_definition_info['cpu'],
+            "memory": task_definition_info['memory']
         })
 
 workbook = xlsxwriter.Workbook('ECS_data.xlsx')
 worksheet = workbook.add_worksheet()
 
-worksheet.write(0, 0, 'Cluster')
-worksheet.write(0, 1, 'Service')
-worksheet.write(0, 2, 'DesiredCount')
-worksheet.write(0, 3, 'LoadBalancer Target Group')
+cell_format = workbook.add_format({'bold': True, 'font_color': 'brown'})
 
+worksheet.write(0, 0, 'Cluster', cell_format)
+worksheet.write(0, 1, 'Service', cell_format)
+worksheet.write(0, 2, 'DesiredCount', cell_format)
+worksheet.write(0, 3, 'LoadBalancer Target Group', cell_format)
+worksheet.write(0, 4, 'Task Definition', cell_format)
+worksheet.write(0, 5, 'CPU', cell_format)
+worksheet.write(0, 6, 'Memory', cell_format)
 row_num = 1
+
 for i in data:
     print(i)
     worksheet.write(row_num, 0, i['cluster'])
@@ -38,6 +48,10 @@ for i in data:
         worksheet.write(row_num, 3, str(i['load_balancer']).split('/')[1])
     else:
         worksheet.write(row_num, 3, '')
+    worksheet.write(row_num, 4, i['task_definition'])
+    worksheet.write(row_num, 5, i['cpu'])
+    worksheet.write(row_num, 6, i['memory'])
+    
     row_num += 1
 
 workbook.close()
