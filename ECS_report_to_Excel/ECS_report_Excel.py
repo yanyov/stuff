289e782e6d03133273dc522a2ba022ce07d55b5b
Asen Yanyov <asen.yanyov@scalefocus.com>
2021-02-10 14:07:18 +0200

    update script

--- ECS_report_to_Excel/ECS_report_Excel.py
+++ ECS_report_to_Excel/ECS_report_Excel.py
@@ -1,5 +1,6 @@
 import boto3
 import xlsxwriter
+import pprint
 
 client = boto3.client('ecs')
 ecs_clusters = client.list_clusters().get('clusterArns')
@@ -22,7 +23,9 @@ for cluster, services in ecs_services.items():
             "load_balancer": desc_serv[0]['loadBalancers'],
             "task_definition": task_definition,
             "cpu": task_definition_info['cpu'],
-            "memory": task_definition_info['memory']
+            "memory": task_definition_info['memory'],
+            "region": task_definition_info['containerDefinitions'][0]['logConfiguration']['options']['awslogs-region'],
+            "log_group": task_definition_info['containerDefinitions'][0]['logConfiguration']['options']['awslogs-group']
         })
 
 workbook = xlsxwriter.Workbook('ECS_data.xlsx')
@@ -37,6 +40,9 @@ worksheet.write(0, 3, 'LoadBalancer Target Group', cell_format)
 worksheet.write(0, 4, 'Task Definition', cell_format)
 worksheet.write(0, 5, 'CPU', cell_format)
 worksheet.write(0, 6, 'Memory', cell_format)
+worksheet.write(0, 7, 'Region', cell_format)
+worksheet.write(0, 8, 'AWS Cloudwatch Log Group', cell_format)
+
 row_num = 1
 
 for i in data:
@@ -51,6 +57,8 @@ for i in data:
     worksheet.write(row_num, 4, i['task_definition'])
     worksheet.write(row_num, 5, i['cpu'])
     worksheet.write(row_num, 6, i['memory'])
+    worksheet.write(row_num, 7, i['region'])
+    worksheet.write(row_num, 8, i['log_group'])
     
     row_num += 1
 
